38d620dfd747810a087305be5f5d3263cf59487d
Ashish Kumar Yadav <ay9537155@gmail.com>
2024-02-28 16:56:09 +0530

    adding script code

---
+++ script.js
@@ -0,0 +1,38 @@
+let box1=document.getElementById("box-1");
+let box2=document.getElementById("box-2");
+let box3=document.getElementById("box-3");
+let box4=document.getElementById("box-4");
+let box5=document.getElementById("box-5");
+let box6=document.getElementById("box-6");
+let box7=document.getElementById("box-7");
+let box8=document.getElementById("box-8");
+let box9=document.getElementById("box-9");
+
+box1.addEventListener('click',function(){
+    box1.innerText = "X"
+});
+box2.addEventListener('click',function(){
+    box2.innerText = "O"
+});
+box3.addEventListener('click',function(){
+    box3.innerText = "X"
+});
+box4.addEventListener('click',function(){
+    box4.innerText = "O"
+});
+box5.addEventListener('click',function(){
+    box5.innerText = "X"
+});
+box6.addEventListener('click',function(){
+    box6.innerText = "O"
+});
+box7.addEventListener('click',function(){
+    box7.innerText = "X"
+});
+
+box8.addEventListener('click',function(){
+    box8.innerText = "O"
+});
+box9.addEventListener('click',function(){
+    box9.innerText = "X"
+});
