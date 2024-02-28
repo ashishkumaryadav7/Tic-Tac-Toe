1505054471b1e896ea34c544cc7c0664e8947062
Ashish Kumar Yadav <ay9537155@gmail.com>
2024-02-28 23:06:23 +0530

    change in JS

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-let box1=document.getElementById("box-1");
-let box2=document.getElementById("box-2");
-let box3=document.getElementById("box-3");
-let box4=document.getElementById("box-4");
-let box5=document.getElementById("box-5");
-let box6=document.getElementById("box-6");
-let box7=document.getElementById("box-7");
-let box8=document.getElementById("box-8");
-let box9=document.getElementById("box-9");
+let boxes = document.querySelectorAll('.sub-box');
+let turnVal = 'O';
 
-box1.addEventListener('click',function(){
-    box1.innerText = "X"
-});
-box2.addEventListener('click',function(){
-    box2.innerText = "O"
-});
-box3.addEventListener('click',function(){
-    box3.innerText = "X"
-});
-box4.addEventListener('click',function(){
-    box4.innerText = "O"
-});
-box5.addEventListener('click',function(){
-    box5.innerText = "X"
-});
-box6.addEventListener('click',function(){
-    box6.innerText = "O"
-});
-box7.addEventListener('click',function(){
-    box7.innerText = "X"
+
+boxes.forEach((box) => {
+    box.clicked = false;
+    box.addEventListener('click', function() {
+        if (!box.clicked) {
+            fillBox(box);
+            box.clicked = true;
+        }
+    });
 });
 
-box8.addEventListener('click',function(){
-    box8.innerText = "O"
-});
-box9.addEventListener('click',function(){
-    box9.innerText = "X"
-});
+function fillBox(e){
+    turn(turnVal);
+    e.innerText = turnVal;
+}
+
+function turn(val){
+    turnVal = (val != 'X') ? "X":"O"
+    console.log(turnVal);
+}
