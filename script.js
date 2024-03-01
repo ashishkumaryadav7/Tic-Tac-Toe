177b8aa8c74aca3486cb589d4c88e77d6c12e24b
Ashish Kumar Yadav <ay9537155@gmail.com>
2024-03-01 14:30:21 +0530

    Adding JS code update

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let boxes = document.querySelectorAll('.sub-box');
 let messageBox = document.querySelector(".Message-box");
 let result = document.querySelector(".result");
 let turnVal = 'O';
+let boxc1 = 0;
+let boxc2 = 0;
+let boxc3 = 0;
 
 let pattern = [
     ["box-1","box-2","box-3"],
@@ -60,28 +63,65 @@ function checkWiner(){
 function displayWinner(val){
     if(val != 0){
         console.log(val + "Winnner");
-        result.style.display = 'block';
+        result.style.visibility = 'visible';
         result.innerText = `${val} is Winner`;
     }
     else{
         console.log("Draw");
-        result.style.display = 'block';
+        result.style.visibility = 'visible';
         result.innerText = "Draw !";
     }
+    toDisableAfterWinnig();
 }
+
 function announcWinner(i1,i2,i3){
     id1=document.getElementById(i1);
     id2=document.getElementById(i2);
     id3=document.getElementById(i3);
 
-    id1.style.backgroundColor = "#19D397";
-    id2.style.backgroundColor = "#19D397";
-    id3.style.backgroundColor = "#19D397";
-}
+    boxc1 = id1;
+    boxc2 = id2;
+    boxc3 = id3;
 
+    id1.style.backgroundColor = "#2eb98b";
+    id2.style.backgroundColor = "#2eb98b";
+    id3.style.backgroundColor = "#2eb98b";
+}
 
 
 function turn(val){
     messageBox.innerText = `${turnVal}'s Turn`; 
     turnVal = (val != 'X') ? "X":"O"
+}
+
+function toDisableAfterWinnig(){
+    boxes.forEach((box) => {    
+        box.clicked = true;
+    });
+    messageBox.innerText = 'New Game';
+    messageBox.style.cursor = 'pointer';
+    newGame();
+    
+}
+
+
+function newGame() {
+    var clickFunction = function() {
+        result.style.visibility = 'hidden';
+        boxc1.style.backgroundColor = '#39495a31';
+        boxc2.style.backgroundColor = '#39495a31';
+        boxc3.style.backgroundColor = '#39495a31';
+        turnVal = 'O';
+        messageBox.innerText = "X's Turn";
+        messageBox.style.cursor = 'auto';
+        boxes.forEach((box) => {
+            box.clicked = false;
+            box.innerText= '';
+        });
+        // Remove the event listener
+        messageBox.removeEventListener('click', clickFunction);
+    }
+
+    // Add the event listener
+    messageBox.addEventListener('click', clickFunction);
 }
